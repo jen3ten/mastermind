268e53d7372ba7d7eeecb7a772f451593685ff4b
Jennifer Evans <jenevans3ten@gmail.com>
2021-01-02 13:35:17 -0500

    introductory message and rules presented to player

--- mastermind/Program.cs
+++ mastermind/Program.cs
@@ -6,7 +6,15 @@ namespace mastermind
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Mastermind!\n");
+            Console.WriteLine("Your goal is to guess the 4 digit answer I have generated.\n");
+            Console.WriteLine("RULES:");
+            Console.WriteLine("Each digit is an integer between 1-6.");
+            Console.WriteLine("Digits can be repeated.");
+            Console.WriteLine("You will have 10 chances to guess the answer.");
+            Console.WriteLine("I will give you a - (minus) symbol for every digit that is correct, but in the wrong position.");
+            Console.WriteLine("I will give you a + (plus) symbol for every digit that is correct and in the correct position.");
+            Console.WriteLine("\nGood Luck!\n");
         }
     }
 }
