533e15171df298d0213a4663ee15798a90b010b9
Jennifer Evans <jenevans3ten@gmail.com>
2021-01-17 16:08:41 -0500

    added new UserInterface class with DisplayRules static method

--- mastermind/Program.cs
+++ mastermind/Program.cs
@@ -6,17 +6,17 @@ namespace mastermind
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Mastermind!\n");
-            Console.WriteLine("Your goal is to guess the 4 digit answer I have generated.\n");
-            Console.WriteLine("RULES:");
-            Console.WriteLine("Each digit is an integer between 1-6.");
-            Console.WriteLine("Digits can be repeated.");
-            Console.WriteLine("You will have 10 chances to guess the answer.");
-            Console.WriteLine("I will give you a + (plus) symbol for every digit that is correct and in the correct position.");
-            Console.WriteLine("I will give you a - (minus) symbol for every digit that is correct, but in the wrong position.");
-            Console.WriteLine("Enter exactly 4 digits for each guess. Do not include spaces or commas.");
-            Console.WriteLine("\nGood Luck!\n");
-
+            //Console.WriteLine("Mastermind!\n");
+            //Console.WriteLine("Your goal is to guess the 4 digit answer I have generated.\n");
+            //Console.WriteLine("RULES:");
+            //Console.WriteLine("Each digit is an integer between 1-6.");
+            //Console.WriteLine("Digits can be repeated.");
+            //Console.WriteLine("You will have 10 chances to guess the answer.");
+            //Console.WriteLine("I will give you a + (plus) symbol for every digit that is correct and in the correct position.");
+            //Console.WriteLine("I will give you a - (minus) symbol for every digit that is correct, but in the wrong position.");
+            //Console.WriteLine("Enter exactly 4 digits for each guess. Do not include spaces or commas.");
+            //Console.WriteLine("\nGood Luck!\n");
+            UserInterface.DisplayRules();
             // Initialize new game and generate random answer
             Game game = new Game();
             game.GenerateRandomAnswer();
---
+++ mastermind/UserInterface.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mastermind
+{
+    public class UserInterface
+    {
+        public static void DisplayRules()
+        {
+            Console.WriteLine("Mastermind!\n");
+            Console.WriteLine("Your goal is to guess the 4 digit answer I have generated.\n");
+            Console.WriteLine("RULES:");
+            Console.WriteLine("Each digit is an integer between 1-6.");
+            Console.WriteLine("Digits can be repeated.");
+            Console.WriteLine("You will have 10 chances to guess the answer.");
+            Console.WriteLine("I will give you a + (plus) symbol for every digit that is correct and in the correct position.");
+            Console.WriteLine("I will give you a - (minus) symbol for every digit that is correct, but in the wrong position.");
+            Console.WriteLine("Enter exactly 4 digits for each guess. Do not include spaces or commas.");
+            Console.WriteLine("\nGood Luck!\n");
+        }
+    }
+}
