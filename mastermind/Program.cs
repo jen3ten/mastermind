69624b5f9cfed818aacfd0e2c8756a70b84235e0
Jennifer Evans <jenevans3ten@gmail.com>
2021-01-17 16:28:18 -0500

    remove commented code from Program class

--- mastermind/Program.cs
+++ mastermind/Program.cs
@@ -6,16 +6,6 @@ namespace mastermind
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Mastermind!\n");
-            //Console.WriteLine("Your goal is to guess the 4 digit answer I have generated.\n");
-            //Console.WriteLine("RULES:");
-            //Console.WriteLine("Each digit is an integer between 1-6.");
-            //Console.WriteLine("Digits can be repeated.");
-            //Console.WriteLine("You will have 10 chances to guess the answer.");
-            //Console.WriteLine("I will give you a + (plus) symbol for every digit that is correct and in the correct position.");
-            //Console.WriteLine("I will give you a - (minus) symbol for every digit that is correct, but in the wrong position.");
-            //Console.WriteLine("Enter exactly 4 digits for each guess. Do not include spaces or commas.");
-            //Console.WriteLine("\nGood Luck!\n");
             UserInterface.DisplayRules();
             // Initialize new game and generate random answer
             Game game = new Game();
@@ -33,9 +23,6 @@ namespace mastermind
                 // Request valid guess from user
                 do
                 {
-                    //string guessPrompt = $"Guess #{guessNumber}:";
-                    //Console.Write($"{guessPrompt, -12}");
-                    //game.ConvertInputToStringArray(Console.ReadLine());
                     string guess = UserInterface.ReadGuess(guessNumber);
                     game.ConvertInputToStringArray(guess);
                 } while (!game.CheckValidityOfInput());
@@ -48,7 +35,6 @@ namespace mastermind
                 {
                     continueGuessing = false;
                     UserInterface.WinningMessage(guessNumber);
-                    //Console.WriteLine($"Congratulations!  You guessed the answer in {guessNumber} tries!");
                 }
                 // If not a winning guess, identify number of digits that match answer and display result
                 else
@@ -56,11 +42,10 @@ namespace mastermind
                     game.CompareCorrectDigit();
                     game.DisplayResult();
 
-                    // Disply message and stop game if maximum number of guesses have been reached
+                    // Display message and stop game if maximum number of guesses have been reached
                     if(guessNumber >= game.MaximumGuesses)
                     {
                         continueGuessing = false;
-                        //Console.WriteLine($"Too bad!  You didn't guess the answer in {game.MaximumGuesses} tries.");
                         UserInterface.LosingMessage(game.MaximumGuesses);
                         game.DisplayAnswer();
                     }
